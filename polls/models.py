4849602d5b536039fb1c5ca6a3a106b31ae18648
Hoang3402 <nvh.02021995@gmail.com>
2024-02-26 12:32:48 +0700

    Init polls

---
+++ polls/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+
+
+class Hotel(models.Model):
+    pass
+
+
+class Room(models.Model):
+    pass
+
+
+class Booking(models.Model):
+    pass
+
+
+class User(models.Model):
+    pass
+
+
+class Review(models.Model):
+    pass
+
+
