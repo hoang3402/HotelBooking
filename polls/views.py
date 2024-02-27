beb1dcee2c44f42c00145c36354b0db9141df52a
Hoang3402 <nvh.02021995@gmail.com>
2024-02-27 11:19:26 +0700

    Implement basic get

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,14 @@
-from django.http import JsonResponse
+from rest_framework import permissions
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.response import Response
+
+from polls.models import Hotel
+from polls.serializers import HotelSerializer
 
 
+@api_view(['GET'])
+@permission_classes((permissions.AllowAny,))
 def index(request):
-    return JsonResponse({'message': 'Hello World!'})
-
+    data_query = Hotel.objects.all()
+    serializer = HotelSerializer(data_query, many=True)
+    return Response(serializer.data)
