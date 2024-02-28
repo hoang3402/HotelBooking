aa734841310578788faab8233951f7054efe3ff9
Hoang3402 <nvh.02021995@gmail.com>
2024-02-28 15:17:16 +0700

    remove unused

--- polls/serializers.py
+++ polls/serializers.py
@@ -1,21 +1,2 @@
-from rest_framework import serializers
-
-from .models import Hotel, User
 
 
-class HotelSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Hotel
-        fields = '__all__'
-
-
-class UserSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True)
-
-    class Meta:
-        model = User
-        fields = ['email', 'first_name', 'last_name', 'number_phone', 'password']
-
-    def create(self, validated_data):
-        user = User.objects.create_user(**validated_data)
-        return user
