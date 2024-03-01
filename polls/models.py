632536e6cc9eb91d7b405b1af0a5813160de504f
Hoang3402 <nvh.02021995@gmail.com>
2024-03-01 08:45:09 +0700

    update db

--- polls/models.py
+++ polls/models.py
@@ -34,6 +34,7 @@ class Room(models.Model):
     maximum_occupancy = models.IntegerField(default=1)
     price = models.DecimalField(max_digits=102, decimal_places=2)
     image = models.CharField(max_length=255)
+    is_available = models.BooleanField(default=True)
 
     hotel = models.ForeignKey(Hotel, on_delete=models.CASCADE)
     room_type = models.ForeignKey(RoomType, on_delete=models.CASCADE)
@@ -141,3 +142,34 @@ class Review(models.Model):
 
     def __str__(self):
         return f'{self.id} - {self.title}'
+
+
+class City(models.Model):
+    code = models.CharField(max_length=20, primary_key=True)
+    name = models.CharField(max_length=255)
+    country_code = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class Country(models.Model):
+    code = models.CharField(max_length=20, primary_key=True)
+    name = models.CharField(max_length=255)
+    currency = models.CharField(max_length=18)
+
+    def __str__(self):
+        return self.name
+
+
+class HotelFeatures(models.Model):
+    code = models.CharField(max_length=20, primary_key=True)
+    description = models.CharField(max_length=255, null=True)
+
+    def __str__(self):
+        return self.code
+
+
+class SpecificHotelFeature(models.Model):
+    hotel = models.ForeignKey(Hotel, on_delete=models.CASCADE)
+    feature = models.ForeignKey(HotelFeatures, on_delete=models.CASCADE)
