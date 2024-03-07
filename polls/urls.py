6b1d1e8726a23a6e989a99290ef8c7c97c9ff2c7
Hoang3402 <nvh.02021995@gmail.com>
2024-03-07 09:54:49 +0700

    Fix typo

--- polls/urls.py
+++ polls/urls.py
@@ -19,35 +19,35 @@ urlpatterns = [
 
     path('room-type/', views.room_type_list_view, name='room_type_list_view'),
     path('room-type/create/', views.room_type_create_view, name='room_type_create_view'),
-    path('room-type/<str:pk>/edit/', views.room_type_edit_view, name='room_type_create_view'),
-    path('room-type/<str:pk>/delete/', views.room_type_delete_view, name='room_type_create_view'),
+    path('room-type/<str:pk>/edit/', views.room_type_edit_view, name='room_type_edit_view'),
+    path('room-type/<str:pk>/delete/', views.room_type_delete_view, name='room_type_delete_view'),
 
     path('feature/', views.feature_list_view, name='feature_list_view'),
     path('feature/create/', views.feature_create_view, name='feature_create_view'),
-    path('feature/<str:pk>/edit/', views.feature_edit_view, name='feature_create_view'),
-    path('feature/<str:pk>/delete/', views.feature_delete_view, name='feature_create_view'),
+    path('feature/<str:pk>/edit/', views.feature_edit_view, name='feature_edit_view'),
+    path('feature/<str:pk>/delete/', views.feature_delete_view, name='feature_delete_view'),
 
     path('feature-hotel/', views.feature_hotel_list_view, name='feature_hotel_list_view'),
     path('feature-hotel/<int:pk>/', views.feature_hotel_detail_view, name='feature_hotel_list_view'),
     path('feature-hotel/create/', views.feature_hotel_create_view, name='feature_hotel_create_view'),
-    path('feature-hotel/<str:pk>/edit/', views.feature_hotel_edit_view, name='feature_hotel_create_view'),
-    path('feature-hotel/<str:pk>/delete/', views.feature_hotel_delete_view, name='feature_hotel_create_view'),
+    path('feature-hotel/<str:pk>/edit/', views.feature_hotel_edit_view, name='feature_hotel_edit_view'),
+    path('feature-hotel/<str:pk>/delete/', views.feature_hotel_delete_view, name='feature_hotel_delete_view'),
 
     path('city/', views.city_list_view, name='city_list_view'),
     path('city/create/', views.city_create_view, name='city_create_view'),
-    path('city/<str:pk>/edit/', views.city_edit_view, name='city_create_view'),
-    path('city/<str:pk>/delete/', views.city_delete_view, name='city_create_view'),
+    path('city/<str:pk>/edit/', views.city_edit_view, name='city_edit_view'),
+    path('city/<str:pk>/delete/', views.city_delete_view, name='city_delete_view'),
 
     path('country/', views.country_list_view, name='country_list_view'),
     path('country/create/', views.country_create_view, name='country_create_view'),
-    path('country/<str:pk>/edit/', views.country_edit_view, name='country_create_view'),
-    path('country/<str:pk>/delete/', views.country_delete_view, name='country_create_view'),
+    path('country/<str:pk>/edit/', views.country_edit_view, name='country_edit_view'),
+    path('country/<str:pk>/delete/', views.country_delete_view, name='country_delete_view'),
 
     path('booking/', views.booking_list_view, name='booking_list_view'),
     path('booking/<int:pk>/', views.booking_detail_view, name='booking_list_view'),
     path('booking/create/', views.booking_create_view, name='booking_create_view'),
-    path('booking/<str:pk>/edit/', views.booking_edit_view, name='booking_create_view'),
-    path('booking/<str:pk>/delete/', views.booking_delete_view, name='booking_create_view'),
+    path('booking/<str:pk>/edit/', views.booking_edit_view, name='booking_edit_view'),
+    path('booking/<str:pk>/delete/', views.booking_delete_view, name='booking_delete_view'),
 
     path('search/', views.search_view, name="search"),
     path('make-booking/', views.make_booking_view, name="make_booking")
